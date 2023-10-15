bc62ccbe34035b67eb6f489027dec401b58135a9
Tomas Duchek <neb7pxuk@duchic.anonaddy.com>
2023-10-15 22:10:37 +0200

    read ip address from file and ping them

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,40 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final String APP_TOKEN = args[0];
+        final String USER_TOKEN = args[1];
+        readIPFromFile();
+    }
+
+    private static void readIPFromFile() {
+        try {
+            String filePath = "ip.txt";
+            File file = new File(filePath);
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()){
+                String ipAddress = scanner.nextLine();
+                boolean isReachable = ping(ipAddress);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static boolean ping(String ipAddress) {
+        try {
+            InetAddress inetAddress = InetAddress.getByName(ipAddress);
+            return inetAddress.isReachable(3000);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
